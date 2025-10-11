91e70f73a8ae8bb07af18754fc443933641428d1
genaitimestools <hirotobilly@gmail.com>
2025-10-11 11:30:00 +0900

    Update article_script.js

--- My blog/article_script.js
+++ My blog/article_script.js
@@ -1,1 +1,11 @@
+window.addEventListener('DOMContentLoaded', () => {
+  // Code to set the link of the div `article-card-content` the url included in the attribute `value` of the div.
 
+  const articleCardContent = document.getElementsByClassName('article-card-content');
+
+  for(let i = 0; i < articleCardContent.length; i++) {
+    articleCardContent[i].addEventListener('click', () => {
+      window.location.href = articleCardContent[i].getAttribute("value")
+    });
+  }
+});
