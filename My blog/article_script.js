708f2c9b73b916cceb84c35b76021dfdcc4ecdde
genaitimestools <hirotobilly@gmail.com>
2025-10-14 16:01:07 +0900

    Update article_script.js

--- My blog/article_script.js
+++ My blog/article_script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-  // Code to set the link of the div `article-card-content` the url included in the attribute `value` of the div.
+  // Code to set the link of the div `article-card-content` the url included in the attribute `value` of the div
 
   const articleCardContent = document.getElementsByClassName('article-card-content');
 
@@ -9,3 +9,19 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+function activateTab(buttonToActivate, guideToShow) {
+  allButtons.forEach(button => {
+    button.style.border = "none";
+    button.style.backgroundColor = "white";
+  }); 
+
+  allGuides.forEach(guide => {
+    guide.style.display = "none";
+  });
+
+  buttonToActivate.style.border = "2px double black";
+  buttonToActivate.style.borderRadius = "5px 5px 0px 0px";
+  buttonToActivate.style.backgroundColor = "white";
+  guideToShow.style.display = "block";
+}
